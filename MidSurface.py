77ed2fd553518baffe2de1c3c86f20e0eb6a75f1
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-07-03 07:58:30 +0200

    changes before upload

--- MidSurface.py
+++ MidSurface.py
@@ -16,3 +16,35 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+from __future__ import print_function
+
+import salome
+import geompy
+from numpy import array, arange 
+
+from MyGeom.Types import *
+
+def parallelMidsurface(lower_face, upper_face,lower_deg = 2 upper_deg = 5):
+    """
+    Determines the midsurface of 2 parallel
+    surfaces. Hereby parallel means that they
+    share the same normal direction. It is assumed
+    that both normals point outwards.
+    """
+
+    
+    #inverse = geompy.ChangeOrientation(lower_face)
+    #normal = geompy.GetNormal(inverse)
+    parameter_space = arange(0,1,1.0/upper_deg)
+    point_creation = geompy.MakeVertexOnSurface # (arc_face, 0.5, 0.5)
+    # create local coordinate systems
+    points_lower = [point_creation(lower_face,u,v) \
+                        for u in parameter_space for v in parameter_space]
+    points_lower = [MyVertex(p) for p in points_lower] # Transform to MyGeom
+    points_upper = [point_creation(upper_face,u,v) \
+                        for u in parameter_space for v in parameter_space]
+    points_lower = [MyVertex(p) for p in points_upper] # Transform to MyGeom
+    
+    # create midpoints
+    [MyVertex((points_lower[i].getCoord() + points_upper[i].getCoord())/2.) for i in range(len(points_lower))]
