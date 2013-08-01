a1a6d17b30a708df0869494be4e05ba166b2b785
Stefan Reiterer <stefan.reiterer@magnasteyr.com>
2013-08-01 15:32:30 +0200

    added new (untestes algorithm) some corrections

--- SurfaceExtraction.py
+++ SurfaceExtraction.py
@@ -85,7 +85,7 @@ def get_inner_side_of_shell(shell, inner_face, border_faces = []):
                 from the face list. The inner face will then marked as done 
                 Then for each marked face get the neighbours and mark them
                 and then set this face done.
-                This will be done if there are no more faces to mark.
+                This will be until if there are no more faces to mark.
                 
     """
     
@@ -117,11 +117,84 @@ def get_inner_side_of_shell(shell, inner_face, border_faces = []):
         marked += new_neighbours
         done += [marked_face]
 
-        print("Length faces: ",len(face_list)," Length marked: ", len(marked), " Length done: ", len(done))
+        print(" Length marked: ", len(marked), " Length done: ", len(done))
         print(counter)
         counter += 1
         if marked == []:
             return MyShell(done)
 
-    else:
-        raise 
+def filter_list_by_radius(center_face,face_list,radius):
+    inner = []
+    outer = []
+
+    for face in face_list:
+        if get_min_distance(center_face,face) <= radius:
+            inner += [face]
+        else:
+            outer += [face]
+
+    return inner, outer
+
+def get_inner_side_of_shell_with_radius(shell, inner_face, radius, border_faces = []):
+    """
+    Takes a closed shell of faces and return the faces which lie on the
+    inner side. E.g. pipes or tanks etc. This Version needs the faces
+    which serves as border, and one face which lies on the inner
+
+    Algorithm : We remove all borders from the faces. Then get all neighbours
+                of the inner face. These are marked now and will be removed
+                from the face list. The inner face will then marked as done 
+                Then for each marked face get the neighbours and mark them
+                and then set this face done.
+                This will be done until there are no more faces to mark.
+                
+    """
+    
+    if not border_faces:
+        raise NotImplementedError("Error: Automatic determination of borders is not implemented yet!")
+
+    shell = MyShell(shell)
+    face_list = explode_sub_shape(shell,"FACE",add_to_study = False)
+    face_list = [MyFace(face) for face in face_list]
+    # filter out faces which are not in the border list
+    face_list = [face for face in face_list if not face in border_faces]
+    
+    marked = [inner_face]
+    done = []
+    counter = 0
+    while True:
+        
+        filtered_face_list, face_list = filter_list_by_radius(marked[-1],face_list,radius)
+        while True:
+            marked_face = marked.pop()
+            # get neigbour indices
+            indices = [i for i in range(len(face_list)) \
+                       if check_neighbour(marked_face,filtered_face_list[i]) is True]
+        
+
+            # filter out new_neighbours
+            new_neighbours = [filtered_face_list[i] for i in indices ]
+            for i in indices:
+                filtered_face_list[i] = None
+
+                marked += new_neighbours
+                done += [marked_face]
+
+                print(" Length marked: ", len(marked), " Length done: ", len(done))
+                print(counter)
+                counter += 1
+                if marked == []:
+                    break
+
+        for done_face in done:
+            for face in face_list:
+                if check_neighbour(done_face,face) is True:
+                    marked += [face]
+                    found = True
+                    break
+            if found:
+                break
+        else:
+            return done
+                
+            
